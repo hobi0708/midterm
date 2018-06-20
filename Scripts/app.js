4021bb568f5f33eb66d166c2de292875563803c3
Jongyoon Ko <hobi0708@gmail.com>
2018-06-20 11:53:33 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+// setup your IIFE (Immediately Invoked Function Expression)
+(function () {
+"use strict";
+
+
+// YOUR CODE GOES HERE
+
+var IntroParagraph = prompt("Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals. Greece has 1,400 islands, though only 230 of them are inhabited. And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too."); 
+
+    document.getElementById('IntroParagraph').innerHTML = IntroParagraph;
+})
+
